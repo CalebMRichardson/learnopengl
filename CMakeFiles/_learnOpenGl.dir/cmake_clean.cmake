a73afdf3b0d3286c0997a9550e6ad7b21bcca614
Caleb Richardson <cmrichardson1993@outlook.com>
2018-11-14 21:29:37 -0800

    Updated CMakeList to support linux

---
+++ CMakeFiles/_learnOpenGl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_learnOpenGl.dir/main.cpp.o"
+  "CMakeFiles/_learnOpenGl.dir/glad.c.o"
+  "_learnOpenGl.pdb"
+  "_learnOpenGl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/_learnOpenGl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
